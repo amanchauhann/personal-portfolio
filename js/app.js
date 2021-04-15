f27858a214537fb703bf560d169b2330a73e8991
RuariDouglas <70585404+RuariDouglas@users.noreply.github.com>
2021-04-15 20:34:38 +0200

    Add about section, tweak styling

--- js/app.js
+++ js/app.js
@@ -1,63 +1,88 @@
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+};
+
 const menuIcon = document.querySelector(".menu-icon");
 const menuList = document.querySelector(".menu-list");
+const aboutSection = document.querySelector(".about");
+const readMore = document.querySelector(".read-more__link");
+const homeLink = document.querySelector(".home-link");
+let toggle = false;
 menuIcon.addEventListener("click", (e) => {
   menuList.classList.toggle("open");
+});
+readMore.addEventListener("click", (e) => {
+  aboutSection.classList.toggle("about__open");
+  if (!toggle) {
+    readMore.textContent = "That's plenty!";
+    toggle = true;
+  } else {
+    readMore.textContent = "About Me";
+    toggle = false;
+  }
 });
 
 const emailLi = document.querySelector(".email-li");
 
 const timeline = gsap.timeline({ defaults: { duration: 1 } });
-
-// TOP NAV
-gsap.from(".top-nav", { y: "-100%", delay: 0.5 });
-gsap.from(".menu-list li", {
-  duration: 1,
-  y: "-100%",
-  delay: 1,
-  stagger: 0.5,
-  ease: "power",
-});
-
-// SIDE NAV
-gsap.from(".side-nav", { x: "-100%", delay: 1 });
-gsap.from(".side-nav ul li", {
-  duration: 1,
-  x: "-100%",
-  delay: 1,
-  stagger: 0.5,
-  ease: "power",
-});
-
-// WRAPPER
-gsap.from(".heading-text1", { opacity: 0, delay: 1.5 });
-gsap.from(".heading-text2", { opacity: 0, delay: 1.7 });
-gsap.from(".heading-line", { y: "10px", opacity: 0, delay: 1.7, duration: 1 });
-
-// PORTFOLIO
-gsap.from(".entry__title-container", { opacity: 0, delay: 1.5, duration: 1 });
-gsap.from(".entry__img-container", {
-  scale: 0.9,
-  opacity: 0,
-  delay: 1.5,
-  duration: 1,
-});
-gsap.from(".entry__link-container", {
-  opacity: 0,
-  delay: 1.8,
-  duration: 1,
-});
-gsap.from(".entry__about-text", { opacity: 0, delay: 2, duration: 1 });
-gsap.from(".tool-icon", {
-  opacity: 0,
-  y: "-30px",
-  stagger: 0.2,
-  delay: 1.8,
-  duration: 1,
-});
-gsap.from(".tool-icon2", {
-  opacity: 0,
-  y: "-30px",
-  stagger: 0.2,
-  delay: 1.8,
-  duration: 0.5,
-});
+timeline
+  .from(".top-nav", { y: "-100%" }, 0.5)
+  .from(
+    ".menu-list li",
+    {
+      y: "-100%",
+      stagger: 0.2,
+      ease: "power",
+    },
+    1
+  )
+  .from(".side-nav", { x: "-100%" }, 1)
+  .from(
+    ".side-nav ul li",
+    {
+      x: "-100%",
+      stagger: 0.2,
+      ease: "power",
+    },
+    1
+  )
+  .from(".heading-text1", { opacity: 0 }, 1.5)
+  .from(".heading-text2", { opacity: 0 }, 1.7)
+  .from(".read-more__link", { y: "-20%", opacity: 0, duration: 0.5 }, 1.7)
+  .from(".heading-line", { y: "10px", opacity: 0 }, 1.7)
+  .from(".entry__title-container", { opacity: 0 }, 1.5)
+  .from(
+    ".entry__img-container",
+    {
+      scale: 0.9,
+      opacity: 0,
+    },
+    1.5
+  )
+  .from(
+    ".entry__link-container",
+    {
+      opacity: 0,
+    },
+    1.8
+  )
+  .from(
+    ".tool-icon",
+    {
+      opacity: 0,
+      y: "-30px",
+      stagger: 0.2,
+    },
+    1.8
+  )
+  .from(".entry__about-text", { opacity: 0 }, 2)
+  .from(
+    ".tool-icon2",
+    {
+      opacity: 0,
+      y: "-30px",
+      stagger: 0.2,
+      duration: 0.5,
+    },
+    1.8
+  );
